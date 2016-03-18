4ebd8c51ec1d51f7d4d1dc385aceff4d7c957328
Ashley Childers <ashley.childers@gmail.com>
2016-03-18 09:56:55 -0400

    Add text for click counter

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 heading.addEventListener( 'click', tally );
 
 function tally () {
-    heading.innerHTML=
-    '<p onClick="hi()">This is click number <a id="clicks">1</a></p>';
-}
+    heading.innerHTML =
+    '<p>This is click number <a id="clicks">1</a></p>';
+
     var clicks = 0;
     function hi () {
         clicks += 1;
         document.getElementbyId( 'clicks' ).innerHTML = clicks;
+    }
+
 }
