7dd718384f1ec3a78c9917b6c781e43367f829bd
Ashley Childers <ashley.childers@gmail.com>
2016-03-18 14:51:55 -0400

    Update to text in listener branch

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
+var selectedElement = document.getElementById( 'heading' );
+var newDOMElement = document.createElement( 'article' );
+
 heading.addEventListener( 'click', tally );
 
 function tally () {
-    heading.innerHTML =
-    '<p>This is click number <a id="clicks">1</a></p>';
+    newDOMElement.innerHTML = '<p>This is click number <a id="clicks">1</a></p>';
+    selectedElement.appendChild( newDOMElement );
 
     var clicks = 0;
-    function hi () {
+    function onclick () {
         clicks += 1;
         document.getElementbyId( 'clicks' ).innerHTML = clicks;
     }
