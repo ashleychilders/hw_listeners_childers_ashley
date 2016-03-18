81a333bfac4114f586d6acd86715c2e3b1e9867b
Ashley Childers <ashley.childers@gmail.com>
2016-03-18 14:32:31 -0400

    Test options for console.log text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-var submitform = document.getElementById( 'sendform');
-sendform.addEventListener( "submit", function () );{
+var submitform = document.getElementById( 'sendform' );
+var firstNameInput = document.getElementById( 'firstName' );
+var lastNameInput = document.getElementById( 'lastName' );
+var emailInput = document.getElementById( 'email' );
+var messageInput = document.getElementById( 'message' );
 
-}
+submitform.addEventListener( 'submit', function( event ) {
+    event.preventDefault();
+    console.log( 'The firstName is:' + firstNameInput.value );
+    console.log( 'The lastName is:' + lastNameInput.value );
+    console.log( 'The email is:' + emailInput.value );
+    console.log( 'The message is:' + messageInput.value );
+} );
