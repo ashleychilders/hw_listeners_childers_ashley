116e740d5cce00c4376ffe8b7912e8ff7c899b09
Ashley Childers <ashley.childers@gmail.com>
2016-03-18 21:42:19 -0400

    Attempt to correct counter

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+function newtext() {
+    var selectedElement = document.getElementById( 'heading' );
+    var newDOMElement = document.createElement( 'article' );
+    newDOMElement.innerHTML = ( '<p>This is click number</p><p id="clicks">1</p>' );
+    selectedElement.appendChild( newDOMElement );
+}
+heading.addEventListener( 'click', newtext );
+
 var counter = 1;
 function add() {
     return counter += 1;
@@ -6,11 +14,3 @@ function add() {
 function myFunction () {
     document.getElementById( 'clicks' ).innerHTML = add();
 }
-
-function newtext() {
-    var selectedElement = document.getElementById( 'heading' );
-    var newDOMElement = document.createElement( 'article' );
-    newDOMElement.innerHTML = ( '<p>This is click number</p><p id="clicks">1</p>' );
-    selectedElement.appendChild( newDOMElement );
-}
-heading.addEventListener( 'click', newtext );
