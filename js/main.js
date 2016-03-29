a1527c1bcfa87b10af2222924b2f2dea2517332b
Richard Kalehoff <richardkalehoff@gmail.com>
2016-03-28 21:05:44 -0700

    append new content to the body

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 function newtext() {
-    var selectedElement = document.getElementById( 'heading' );
     var newDOMElement = document.createElement( 'article' );
     newDOMElement.innerHTML = ( '<p>This is click number</p><p id="clicks">1</p>' );
-    selectedElement.appendChild( newDOMElement );
+    document.body.appendChild( newDOMElement );
 }
 heading.addEventListener( 'click', newtext );
 
