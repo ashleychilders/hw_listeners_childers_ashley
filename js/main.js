3edae1aa4ce42a1f228429a24351d43e0a55470a
Richard Kalehoff <richardkalehoff@gmail.com>
2016-03-28 21:07:09 -0700

    print out a number with each click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function newtext() {
     var newDOMElement = document.createElement( 'p' );
-    newDOMElement.innerHTML = 'This is click number x';
+    newDOMElement.innerHTML = 'This is click number ' + counter;
     document.body.appendChild( newDOMElement );
 }
 heading.addEventListener( 'click', newtext );
